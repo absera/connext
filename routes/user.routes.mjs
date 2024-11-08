2589c6473f2888b748e032fc911a59017c727efd
absera <abseratemesgen@gmail.com>
2024-11-08 09:23:36 -0500

    user profile added

--- routes/user.routes.mjs
+++ routes/user.routes.mjs
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import * as userServices from '../services/user.service.mjs'
 const userRouter = Router();
 
-userRouter.get('/users/:user_net_id', (req, res) => {
-    
-    res.render('user', { user: req.session.user })
+userRouter.get('/users/:user_net_id', async (req, res) => {
+    const courses = await userServices.getEnrolledCourses(req.params.user_net_id)
+    res.render('user', { user: req.session.user, courses: courses })
 });
 
 export default userRouter;
