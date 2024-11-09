375c616b12150a1ca091a54297a075aea1c66360
absera <abseratemesgen@gmail.com>
2024-11-08 22:20:51 -0500

    basic router change

--- routes/feed.routes.mjs
+++ routes/feed.routes.mjs
@@ -1,7 +1,13 @@
 import { Router } from 'express';
 const feedRouter = Router();
 
-feedRouter.get('/', (req, res) => res.redirect('/feed'));
-feedRouter.get('/feed', (req, res) => res.render('feed', { data: ' ', user: req.session.user }));
+feedRouter.get('/', (req, res) => {
+    res.redirect('/feed')
+});
+
+feedRouter.get('/feed', (req, res) => {
+    res.render('feed', { data: ' ', user: req.session.user })
+});
+
 
 export default feedRouter;
