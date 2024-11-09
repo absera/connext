818d7c77dafec387b2d5b695d9a1542119ae3c6b
absera <abseratemesgen@gmail.com>
2024-11-08 22:21:33 -0500

    create api endpoint for d3 to get student connection data

--- routes/network.routes.mjs
+++ routes/network.routes.mjs
@@ -1,6 +1,16 @@
 import { Router } from 'express';
+import * as network from '../services/network.service.mjs'
+
 const networkRouter = Router();
 
-networkRouter.get('/network/:user_id', (req, res) => res.render('network', { user: req.session.user }));
+networkRouter.get('/network/user/:user_id', (req, res) => {
+    res.render('network', { user: req.session.user })
+});
+
+networkRouter.get('/network/data', async (req, res) => {
+    const currentUser = req.session.user._id
+    const directUsers = await network.getDirectConnectedUsers(currentUser)
+    res.send({ directUsers: directUsers })
+});
 
 export default networkRouter;
